56cee4d9ed4fe9629880b8485ea6ca28d430da96
chandu2406 <sharma.cms007@gmail.com>
2013-01-14 15:16:06 +0530

    made the pages modular by adding include folder

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php include("include/header.php"); ?>
+
+ <h3>Contact Us</h3>
+  </div>
+
+
+  <div class="row">
+
+    
+    <div class="span12">
+
+      <p> <b>Contact us at: ieep.nitk@gmail.com</b></p>
+  <br />
+  
+    </div>
+
+    
+
+  </div>
+  
+  
+</section>
+
+
+
+<?php include("include/footer.php"); ?>
+    
+
+
+  
+
+</body>
+</html>
---
+++ faq.php
@@ -0,0 +1,29 @@
+<?php include("include/header.php"); ?>
+
+ <h3>FAQs</h3>
+  </div>
+
+
+  <div class="row">
+
+    
+    <div class="span12">
+
+      <p>FAQs.</p>
+  <br />
+  
+    </div>
+
+    
+
+  </div>
+  
+  
+</section>
+
+
+<?php include("include/footer.php"); ?>
+  
+
+</body>
+</html>
